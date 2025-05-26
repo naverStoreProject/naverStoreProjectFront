d7f6799fceec902516d0de5bc2a00d41ff77cbce
winA <wina418@kw.ac.kr>
2025-05-26 14:50:02 +0900

    🎨: 탭바 디자인 수정

--- src/components/layouts/TabBar.vue
+++ src/components/layouts/TabBar.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="bottom-nav">
-    <template v-for="bnlist in bottomNavList">
-      <RouterLink :to="{ name: bnlist.router }" class="bottom-nav__button">
-        <div class="bottom-nav__icon">
-          <img src="" alt="" />
-        </div>
-        <div>
-          <div v-text="bnlist.name" class="text-xs"></div>
-        </div>
-      </RouterLink>
-    </template>
+  <div class="border-t-surface-50 fixed bottom-0 left-0 z-50 w-full border-t bg-white py-2">
+    <div class="mx-auto flex w-3/5 items-center justify-around">
+      <template v-for="bnlist in bottomNavList">
+        <RouterLink :to="{ name: bnlist.router }">
+          <div class="flex flex-col items-center justify-center gap-0.5">
+            <img class="w-7" :src="bnlist.src" :alt="bnlist.name" />
+            <div v-text="bnlist.name" class="text-xs"></div>
+          </div>
+        </RouterLink>
+      </template>
+    </div>
   </div>
   <!-- bottom-nav end -->
 </template>
@@ -17,32 +17,10 @@
 <script setup lang="ts">
 //bottom-nav 리스트
 const bottomNavList = [
-  { name: '홈', router: 'main-home', src: '' },
-  { name: '발견', router: 'home', src: '' },
-  { name: '카테고리', router: 'category', src: '' },
-  { name: '검색', router: 'search', src: '' },
-  { name: '마이쇼핑', router: 'user', src: '' },
+  { name: '홈', router: 'main-home', src: 'src/assets/logo.svg' },
+  { name: '발견', router: 'home', src: 'src/assets/logo.svg' },
+  { name: '카테고리', router: 'category', src: 'src/assets/logo.svg' },
+  { name: '검색', router: 'search', src: 'src/assets/logo.svg' },
+  { name: '마이쇼핑', router: 'user', src: 'src/assets/logo.svg' },
 ]
 </script>
-
-<style>
-.bottom-nav {
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  display: flex;
-  justify-content: space-around;
-  background-color: var(--color-gray-200);
-  height: 5rem;
-  padding: 0rem 2rem 0rem 2rem;
-}
-.bottom-nav__button {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-  max-width: 2.5rem;
-  width: 100%;
-}
-</style>
