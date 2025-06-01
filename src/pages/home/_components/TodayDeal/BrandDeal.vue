e590066d08692584067ef468f67bb611a533ab86
inredzero <caesete0524@naver.com>
2025-06-01 23:43:40 +0900

    🎨design: TodatDeal Layout 구현

---
+++ src/pages/home/_components/TodayDeal/BrandDeal.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="text-center mb-6 mt-20">
+    <div class="text-xl font-semibold mb-2">
+      <p class="text-3xl mb-1 mt-1">브랜드딜 :스페셜</p>
+      대세 브랜드의 다시없는 1DAY 초특가
+    </div>
+  </div>
+
+  <!-- 상품 리스트 출력 -->
+  <div :class="['todayDeals', viewType]">
+    <BrandDealCard
+      v-for="brandDeal in todaydeals"
+      :key="brandDeal.id"
+      :brandDeal="brandDeal"
+      :view-type="viewType"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { todaydeals } from '../../todayDeal'
+import BrandDealCard from './BrandDealCard.vue'
+
+defineProps<{
+  viewType: 'long' | 'middle' | 'small'
+}>()
+</script>
+
+<style scoped>
+.todayDeals {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  padding: 1rem 0.5rem;
+}
+
+.todayDeals.middle > * {
+  width: 88%;
+}
+
+.todayDeals.small > * {
+  width: 45%;
+}
+
+.todayDeals.long {
+  flex-direction: column;
+  align-items: center;
+  height: 20%;
+}
+
+.todayDeals.long > * {
+  width: 100%;
+  max-width: 900px;
+}
+</style>
---
+++ src/pages/home/_components/TodayDeal/SpecialDealCard.vue
@@ -0,0 +1,177 @@
+<template>
+  <div :class="['specialDeal-card', viewType]" v-if="specialDeal">
+    <img :src="specialDeal.image" alt="이미지 없음" />
+    <div class="specialDeal-info">
+      <div class="meta-row">
+        <template v-if="viewType === 'small'">
+          <p class="tag-name-line">
+            <span class="tag-text-wrapper" v-if="specialDeal.adTag">
+              <span class="tag-text">광고</span>&nbsp; </span
+            >{{ specialDeal.name }}
+          </p>
+        </template>
+        <template v-else>
+          <p class="brand-ad-tag-line">
+            <span class="brand">{{ specialDeal.brand }} </span>
+            <span class="ad-tag" v-if="specialDeal.adTag">광고</span>
+          </p>
+          <p class="name">{{ specialDeal.name }}</p>
+        </template>
+      </div>
+
+      <p class="price" v-if="specialDeal.originalPrice">
+        <span v-if="viewType !== 'small'" class="my-price">나의 할인가 </span>
+        <span class="original">{{ specialDeal.originalPrice }}원</span>
+      </p>
+      <p class="price">
+        <span class="discount-rate" v-if="specialDeal.discountRate">{{
+          specialDeal.discountRate
+        }}</span>
+        &nbsp;
+        <span class="current">{{ specialDeal.currentPrice }}원</span>
+      </p>
+
+      <p
+        class="rating"
+        v-if="viewType === 'small' && specialDeal.averageRating && specialDeal.ratingCount"
+      >
+        ⭐ {{ specialDeal.averageRating }} ({{ specialDeal.ratingCount }})
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { TodayDeal } from '../../todayDeal.ts'
+
+const props = defineProps<{
+  specialDeal: TodayDeal
+  viewType: 'long' | 'middle' | 'small'
+}>()
+</script>
+
+<style scoped>
+.specialDeal-card {
+  display: flex;
+  flex-direction: column;
+  padding: 4px;
+  border-radius: 8px;
+  overflow: hidden;
+  background: #fff;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.05);
+  transition: all 0.2s ease;
+}
+
+.specialDeal-card img {
+  width: 100%;
+  aspect-ratio: 1 / 1;
+  object-fit: cover;
+}
+
+.specialDeal-card.long {
+  flex-direction: row;
+  width: 100%;
+  height: 120px;
+  gap: 16px;
+  padding: 0.5rem;
+  box-sizing: border-box;
+  align-items: center;
+}
+
+.specialDeal-card.long img {
+  width: 100px;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 6px;
+}
+
+.specialDeal-card.long .todaySale-info {
+  flex: 1;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.specialDeal-info {
+  padding: 8px;
+}
+
+.brand {
+  font-size: 12px;
+  color: #666;
+}
+
+.price {
+  margin-top: 6px;
+}
+
+.discount-rate {
+  color: #d00;
+  font-weight: bold;
+}
+
+.original {
+  font-size: 12px;
+  color: #aaa;
+  text-decoration: line-through;
+}
+
+.meta-row {
+  align-items: center;
+  gap: 0px;
+  flex-wrap: nowrap; /* 강제로 한 줄 */
+}
+
+.tag-name-line {
+  font-weight: bold;
+  font-size: 15px;
+  line-height: 1.4;
+  overflow-wrap: anywhere;
+  word-break: break-all;
+}
+
+.tag-text-wrapper {
+  display: contents; /* span이 존재하되 레이아웃에 영향 없음 */
+}
+
+.tag-text {
+  background: #eee;
+  padding: 2px 6px;
+  font-size: 12px;
+  border-radius: 4px;
+  color: #333;
+  white-space: nowrap;
+  margin-right: 4px;
+}
+
+.brand-ad-tag-line {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.ad-tag {
+  flex-shrink: 0;
+  background: #eee;
+  padding: 2px 6px;
+  font-size: 12px;
+  border-radius: 4px;
+  color: #333;
+}
+
+.name {
+  flex: 1; /* 나머지 공간 차지 */
+  font-weight: bold;
+  font-size: 15px;
+  overflow: hidden;
+  overflow-wrap: anywhere;
+  text-overflow: ellipsis;
+}
+
+.brand {
+  font-size: 13px;
+  color: #555;
+  width: 100%;
+}
+</style>
