fadfaf8d9795d1efa8336a3cfbd8b7a5331355c4
winA <wina418@kw.ac.kr>
2025-06-05 14:54:43 +0900

    🎨: 색상오류 수정 #49

--- src/pages/category/_components/LeftNav.vue
+++ src/pages/category/_components/LeftNav.vue
@@ -1,16 +1,21 @@
 <template>
-
-    <div ref="navContainer" class="w-1/8 overflow-y-auto bg-white">
-        <div v-for="cat in categories" :key="cat.id" :data-id="cat.id" ref="itemRefs" @click="scrollToCategory(cat.id)"
-            :class="[
-                'p-4 cursor-pointer transition text-xs bg-[var(--color-surface-50)]',
-                selectedCategoryId === cat.id
-                    ? ' bg-white font-[\'GmarketSansBold\']'
-                    : 'hover:bg-[var(--color-surface-300)] text-[var(--color-surface-900)]'
-            ]">
-            {{ cat.name }}
-        </div>
+  <div ref="navContainer" class="w-1/8 overflow-y-auto bg-white">
+    <div
+      v-for="cat in categories"
+      :key="cat.id"
+      :data-id="cat.id"
+      ref="itemRefs"
+      @click="scrollToCategory(cat.id)"
+      :class="[
+        'cursor-pointer bg-[var(--color-surface-50)] p-4 text-xs transition',
+        selectedCategoryId === cat.id
+          ? 'bg-white font-[\'GmarketSansBold\']'
+          : 'text-[var(--color-surface-400)] hover:bg-[var(--color-surface-300)]',
+      ]"
+    >
+      {{ cat.name }}
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -26,34 +31,33 @@ const itemRefs = ref<HTMLElement[]>([])
 const navContainer = ref<HTMLElement | null>(null)
 
 const scrollToCategory = (id: number) => {
-    const el = document.getElementById(`section-${id}`)
-    if (el) {
-        el.scrollIntoView({ behavior: 'smooth', block: 'start' })
-        selectCategory(id)
-    }
+  const el = document.getElementById(`section-${id}`)
+  if (el) {
+    el.scrollIntoView({ behavior: 'smooth', block: 'start' })
+    selectCategory(id)
+  }
 }
 // DOM이 업데이트된 후 스크롤IntoView 메서드가 호출 -> 변경 감지 후 카테고리 스크롤
 // 변경 시 선택한 카테고리로 스크롤
-watch(selectedCategoryId, (newId) => {
-    const container = navContainer.value
-    const el = itemRefs.value.find(el => Number(el.dataset.id) === newId)
+watch(selectedCategoryId, newId => {
+  const container = navContainer.value
+  const el = itemRefs.value.find(el => Number(el.dataset.id) === newId)
 
-    if (el && container) {
-        const elTop = el.offsetTop
-        const elHeight = el.offsetHeight
-        const containerHeight = container.clientHeight
+  if (el && container) {
+    const elTop = el.offsetTop
+    const elHeight = el.offsetHeight
+    const containerHeight = container.clientHeight
 
-        const scrollPosition = elTop - containerHeight / 2 + elHeight / 2
+    const scrollPosition = elTop - containerHeight / 2 + elHeight / 2
 
-        container.scrollTo({
-            top: scrollPosition,
-            behavior: 'smooth'
-        })
-    }
+    container.scrollTo({
+      top: scrollPosition,
+      behavior: 'smooth',
+    })
+  }
 })
 
 onMounted(() => {
-    itemRefs.value = Array.from(document.querySelectorAll('[data-id]')) as HTMLElement[]
+  itemRefs.value = Array.from(document.querySelectorAll('[data-id]')) as HTMLElement[]
 })
-
 </script>
