7950fdbe8dd0b6f04bc7073062b26b938d36934a
laeongmulti <laeongmulticampus@gmail.com>
2025-06-07 19:40:14 +0900

    UPDATE: Main Banner 1차 업데이트

--- src/pages/home/_components/HomeMain.vue
+++ src/pages/home/_components/HomeMain.vue
@@ -1,6 +1,10 @@
 <template>
   <!-- 광고 미정 -->
-  <div class="news"></div>
+  <div class="news">
+    <template v-for="mb in mbData" :key="mb.id">
+      <MainBanner :title="mb.title"></MainBanner>
+    </template>
+  </div>
 
   <!-- 로그인 여부 -->
   <div class="p-2" v-show="isLogin">
@@ -13,9 +17,51 @@
   <!-- 컨텐츠들 -->
 </template>
 
-<script setup>
+<script setup lang="ts">
 import LoginWideBanner from '@/components/layouts/LoginBarBanner.vue'
 import MainShortcutBar from '@/components/layouts/MainShortcutBar.vue'
+
+import { ref, onMounted } from 'vue'
+import MainBanner from '@/pages/home/_components/MainBanner.vue'
+
+const mainBanners = ref([])
+const mbloading = ref(true)
+
+const mbData = ref([]);
+
+
+// 배너 데이터 가져오기
+const fetchMainBanners = async () => {
+  mbloading.value = true;
+
+  try {
+    const response = await fetch('/api/mainBanners', {
+      method: 'GET',
+      headers: {
+        // GET 요청 시 서버에 JSON 형식의 응답을 기대함을 명시
+        'Accept': 'application/json',
+      },
+    });
+    mainBanners.value = await response.json();
+
+    if(mainBanners.value.success == true) {
+      mbData.value = mainBanners.value.data;
+    }
+    else {
+
+    }
+
+  } catch (err) {
+    console.log(err);
+  } finally {
+    mbloading.value = false;
+  }
+};
+
+// 컴포넌트 마운트 시 데이터 가져오기
+onMounted(() => {
+  fetchMainBanners()
+})
 
 //임시 로그인
 const isLogin = true
--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,8 @@ export default defineConfig(({ mode }) => {
         '/api': {
           target: env.VITE_API_URL || 'http://localhost:8080',
           changeOrigin: true,
-          rewrite: (path: string) => path.replace(/^\/api/, ''),
+          // rewrite: (path: string) => path.replace(/^\/api/, ''),
+          rewrite: (path: string) => path,
           secure: false,
         },
       },
