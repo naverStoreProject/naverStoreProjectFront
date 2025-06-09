1a6441e51a013c4763e04b5ca88cd8638c0a6db8
laeongmulti <laeongmulticampus@gmail.com>
2025-06-10 01:02:54 +0900

    UPDATE: 프론트의 임시 api 설정

--- src/pages/home/_components/HomeMain.vue
+++ src/pages/home/_components/HomeMain.vue
@@ -1,8 +1,8 @@
 <template>
   <!-- 광고 미정 -->
   <div class="news">
-    <template v-for="mb in mbData" :key="mb.id">
-      <MainBanner :title="mb.title"></MainBanner>
+    <template v-for="data in mbData" :key="mb.id">
+      <MainBanner :mbanner="data"></MainBanner>
     </template>
   </div>
 
@@ -20,47 +20,25 @@
 <script setup lang="ts">
 import LoginWideBanner from '@/components/layouts/LoginBarBanner.vue'
 import MainShortcutBar from '@/components/layouts/MainShortcutBar.vue'
+import mbanners from '@/api/mainBanner.ts'
 
 import { ref, onMounted } from 'vue'
 import MainBanner from '@/pages/home/_components/MainBanner.vue'
 
-const mainBanners = ref([])
-const mbloading = ref(true)
+const mbData = ref([])
 
-const mbData = ref([]);
-
-
-// 배너 데이터 가져오기
-const fetchMainBanners = async () => {
-  mbloading.value = true;
-
+//데이터 가져오기
+onMounted(async () => {
   try {
-    const response = await fetch('/api/mainBanners', {
-      method: 'GET',
-      headers: {
-        // GET 요청 시 서버에 JSON 형식의 응답을 기대함을 명시
-        'Accept': 'application/json',
-      },
-    });
-    mainBanners.value = await response.json();
-
-    if(mainBanners.value.success == true) {
-      mbData.value = mainBanners.value.data;
+    const response = await mbanners.getAllbanners()
+    if (response.success == true) {
+      mbData.value = response.data
+    } else {
+      console.log(response.message)
     }
-    else {
-
-    }
-
-  } catch (err) {
-    console.log(err);
-  } finally {
-    mbloading.value = false;
+  } catch (error) {
+    console.log(error)
   }
-};
-
-// 컴포넌트 마운트 시 데이터 가져오기
-onMounted(() => {
-  fetchMainBanners()
 })
 
 //임시 로그인
